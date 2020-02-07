483b9470e91353477d5d63c1df0af315ba48ecf6
Michel Lima <michelpl@yahoo.com.br>
2020-02-06 22:45:30 -0300

    :sparkles: Create loading component.

---
+++ src/components/gLobal/BaseSpinner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="base-spinner fa-3x">
+    <i class="fas fa-circle-notch fa-spin"></i>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import "../../assets/scss/variables";
+
+  .base-spinner {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: $dark-medium;
+    color: $featured;
+    z-index: 99999;
+  }
+</style>
+
+<script>
+export default {
+    
+}
+
+</script>
