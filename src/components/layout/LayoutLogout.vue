fe44e5cc1ae218422178f4267914a51bd74535b2
Michel Lima <michelpl@yahoo.com.br>
2020-02-08 17:04:34 -0300

    :sparkles: Add navigation to backoffice page.

---
+++ src/components/layout/LayoutLogout.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="btn btn-logout" @click="logout()">
+    <i class="fa fa-power-off"></i>
+    Sair
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    async logout () {
+      this.$root.$emit('Spinner::show')
+      localStorage.removeItem('mm_token')
+      await this.$router.push({ name: 'login' })
+      this.$root.$emit('Spinner::hide')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .btn-logout {
+    width: 100%;
+    cursor: pointer;
+    color: var(--white);
+    &:hover {
+      color: var(--featured);
+    }
+  }
+</style>
--- src/views/backoffice/Backoffice.vue
+++ src/views/backoffice/Backoffice.vue
@@ -2,6 +2,7 @@
   <div class="row">
     <div class="col-2 navigation-sidebar">
       <h1 class="app-title">Backoffice</h1>
+      <layout-navigation></layout-navigation>
     </div>
     <div class="col-6 home-box">
       <h1>Backoffice</h1>
@@ -10,8 +11,10 @@
 </template>
 
 <script>
+import LayoutNavigation from '../../components/layout/LayoutNavigation'
 export default {
   name: 'Backoffice',
+  components: { LayoutNavigation },
   data: () => ({
     email: '',
     password: '',
