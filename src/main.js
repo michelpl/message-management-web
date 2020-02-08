5e219c8159b76de7b8352515e33e60c077cee559
Michel Lima <michelpl@yahoo.com.br>
2020-02-08 17:08:18 -0300

    :sparkles: Add vue resource

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './assets/scss/app.scss'
 import FirebaseVue from './firebase/index'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
 Vue.use(FirebaseVue)
+Vue.use(VueResource)
 
 new Vue({
   router,
