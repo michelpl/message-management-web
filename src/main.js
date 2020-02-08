c198b1b58d130d753e82925e6e55211f9fbdce4a
Michel Lima <michelpl@yahoo.com.br>
2020-02-08 17:11:12 -0300

    :arrow_down: Remove firebase.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/scss/app.scss'
-import FirebaseVue from './firebase/index'
 import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
-Vue.use(FirebaseVue)
 Vue.use(VueResource)
 
 new Vue({
